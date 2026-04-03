afd83fffe359ba89699dff8618f583128d70bde5
rifkilabbaik <rifkilabbaik@gmail.com>
2026-04-03 15:53:10 +0700

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,11 @@
 const CACHE_NAME = 'sales-report-v1';
-const urlsToCache = ['./', './index.html'];
+const urlsToCache = [
+  '/Report-Sales-Per-2-Jam-2/',
+  '/Report-Sales-Per-2-Jam-2/index.html',
+  '/Report-Sales-Per-2-Jam-2/manifest.json',
+  '/Report-Sales-Per-2-Jam-2/icons/icon-192.png',
+  '/Report-Sales-Per-2-Jam-2/icons/icon-512.png'
+];
 
 self.addEventListener('install', event => {
   event.waitUntil(
