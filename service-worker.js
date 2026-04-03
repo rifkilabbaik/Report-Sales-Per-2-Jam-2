758398187ece973951059916c39a085c030d567a
rifkilabbaik <rifkilabbaik@gmail.com>
2026-04-03 15:57:53 +0700

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,5 @@
 const CACHE_NAME = 'sales-report-v1';
+
 const urlsToCache = [
   '/Report-Sales-Per-2-Jam-2/',
   '/Report-Sales-Per-2-Jam-2/index.html',
@@ -7,14 +8,25 @@ const urlsToCache = [
   '/Report-Sales-Per-2-Jam-2/icons/icon-512.png'
 ];
 
+// Install
 self.addEventListener('install', event => {
   event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll(urlsToCache);
+    })
   );
 });
 
+// Activate (biar langsung aktif)
+self.addEventListener('activate', event => {
+  event.waitUntil(self.clients.claim());
+});
+
+// Fetch (ambil dari cache dulu)
 self.addEventListener('fetch', event => {
   event.respondWith(
-    caches.match(event.request).then(res => res || fetch(event.request))
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
   );
 });
